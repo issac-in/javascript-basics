aab01d29724598ffc563b9ba7ea4608d372c03fb
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-04 20:47:49 -0800

    customize regex code for readability and US

--- 02-fundamentals-part-2/regex-tutorial/validation.js
+++ 02-fundamentals-part-2/regex-tutorial/validation.js
@@ -1,11 +1,11 @@
 // RegEx patterns
 const inputs = document.querySelectorAll('input');
 const patterns = {
-    telephone: /^\d{11}$/,
     username: /^[a-z\d]{5,12}$/i,
-    password: /^[\w@-]{8,20}$/,
-    slug: /^[a-z\d-]{8,20}$/,
     email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
+    password: /^[\w@-]{8,20}$/,
+    telephone: /^\d{10}$/,
+    slug: /^[a-z\d-]{8,20}$/,
 };
 
 // validation function
