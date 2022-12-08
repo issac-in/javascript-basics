84fbce745118e360e8525b9c73340112dda0d4da
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-08 14:29:35 -0800

    Learn that arrays are objects and its elements can be objects too

--- 06-fundamentals-part-4/arrays.js
+++ 06-fundamentals-part-4/arrays.js
@@ -31,3 +31,28 @@
     cars[0] = "Hyundai"; // Changes the value of the first element in cars
 }
 
+// Arrays are Objects
+/**
+ * Arrays are Objects, but are a special type of objects.
+ * The typeof operator in JS returns "object" for arrays.
+ * But, JS arrys are best described AS arrays.
+ * 
+ * Arrays use **numbers** to access its elements.
+ * Regular Objects use **names** to access its members.
+ */
+
+// Array Elements can be Objects
+/**
+ * JS variables can be ojects. Arrays are special kinds of objects.
+ * Because of this, we can have varibles of different types in the
+ * same Array. You can also have objects, functions, and arrays,
+ * all within the same Array
+ */
+() => {
+    function myFunction() {
+        // do something
+    }
+    const cars = ["Toyota", "Honda", "Kia"];
+    const myArray = [Date.now, myFunction(), cars];
+}
+
