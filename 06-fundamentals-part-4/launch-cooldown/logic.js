d14a19ae62ca85ed3f84469bb43f2ab80849bac7
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-08 20:35:20 -0800

    Complete launch countdowne exercise

--- 06-fundamentals-part-4/launch-cooldown/logic.js
+++ 06-fundamentals-part-4/launch-cooldown/logic.js
@@ -1,8 +1,19 @@
 let output = document.querySelector(".output");
 output.innerHTML = '';
 
-// let i = 10;
-
-// const para = document.createElement('p');
-// para.textContent = 'Hello';
-// output.appendChild(para);
+let i = 10;
+while (i >= 0) {
+    const para = document.createElement('p');
+    switch (i) {
+        case 10:
+            para.textContent = "Countdown 10";
+            break;
+        case 0:
+            para.textContent = "Blast off!";
+            break;
+        default:
+            para.textContent = i;
+    }
+    output.appendChild(para);
+    i--;
+}
