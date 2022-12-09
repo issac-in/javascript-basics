875e30cb78a4705596d38b6e8bf383462415c64f
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-08 16:11:35 -0800

    Learn about merging (concatenating) arrays

--- 06-fundamentals-part-4/array-methods.js
+++ 06-fundamentals-part-4/array-methods.js
@@ -20,4 +20,24 @@
      */
     delete cars[1]; // Removes "Honda", leaving an undefined hole in the 1st index of the array.
     cars[1] = "Suzuki"; // Setting "Suzuki" to 1st index of the array to avoid leaving undefined hole.
+
+    /**
+     * Merging (Concatenating) Arrays
+     * 
+     * The concat() method creates a new array by merging (concatenating) existing arrays.
+     * The concat() method does not change the existing arrays. It always returns a new array.
+     * The concat() method can take any number of array arguments.
+     * The concat() method can also take strings as arguments.
+     */
+    () => {
+        const koreanCars = ["Hyundai", "Kia", "GM Korea"];
+        const japaneseCars = ["Toyota", "Honda", "Nissan"];
+        const eastAsianCars = koreanCars.concat(japaneseCars); // Result is ["Hyundai", "Kia", "GM Korea","Toyota", "Honda", "Nissan"]
+
+        const americanCars = ["Jeep", "Ford", "Tesla"];
+        const cars = koreanCars.concat(japaneseCars, americanCars); // Result is ["Hyundai", "Kia", "GM Korea","Toyota", "Honda", "Nissan","Jeep", "Ford", "Tesla"]
+
+        const moreCars = cars.concat("Volkswagen", "Audi", "BMW");
+        // Result is ["Hyundai", "Kia", "GM Korea","Toyota", "Honda", "Nissan","Jeep", "Ford", "Tesla","Volkswagen", "Audi", "BMW"]
+    }
 }
