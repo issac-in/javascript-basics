94c44197f95d4908aa6662d8389b0f11298bf584
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-08 16:02:45 -0800

    Learn about Array operator delete

--- 06-fundamentals-part-4/array-methods.js
+++ 06-fundamentals-part-4/array-methods.js
@@ -12,5 +12,12 @@
     cars.unshift("Toyota"); // adds a new element to beginning of array, "unshifting" older elements, returning new array length of 4
     cars[2] = "Nissan"; // Array elements are accessed using their index number and can be changed like so
     cars[cars.length] = "Subaru"; // Easy way to append a new element to the array
-    
+
+    /**
+     * Array elements can be deleted using the JavaScript operator delete.
+     * Using delete leaves undefined holes in the array.
+     * Preferably, use pop() or shift() instead, whenever possible.
+     */
+    delete cars[1]; // Removes "Honda", leaving an undefined hole in the 1st index of the array.
+    cars[1] = "Suzuki"; // Setting "Suzuki" to 1st index of the array to avoid leaving undefined hole.
 }
