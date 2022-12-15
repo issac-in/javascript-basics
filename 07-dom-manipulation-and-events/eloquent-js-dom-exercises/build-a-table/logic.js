2d9883ee9e8e0776b1622243f2dea2eb5348b8cb
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-14 17:34:45 -0800

    add pseudo code to the exercise

--- 07-dom-manipulation-and-events/eloquent-js-dom-exercises/build-a-table/logic.js
+++ 07-dom-manipulation-and-events/eloquent-js-dom-exercises/build-a-table/logic.js
@@ -8,3 +8,11 @@ const MOUNTAINS = [
     { name: "Mont Blanc", height: 4808, place: "Italy/France" }
 ];
 
+// Initialize the <table>
+// To it, based on the length of MOUNTAINS, initialize that many <tr> children of <table>, + 1
+// Then, initialize the heading cells within the first <tr> child of table, pulling them however we can
+// Then from the 2nd <tr> to the last <tr> child of table,
+// initialize and populate <td> cells with values accordingly
+// Add the <table> to the div with id mountains
+
+// Extra - right align cells that contain number values by setting their style.textAlign property to "right".
